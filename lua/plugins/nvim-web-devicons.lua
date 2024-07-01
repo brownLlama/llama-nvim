2259a3a289b0c10b87b76067ae2698f39d8d1255
brownLlama <inbox.sanu@gmail.com>
2024-07-01 18:05:04 +0200

    nvim-web-devicons added

---
+++ lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,14 @@
+-- Icons for the different file types
+return {
+	"nvim-tree/nvim-web-devicons",
+	config = function()
+		require("nvim-web-devicons").set_icon({
+			gql = {
+				icon = "",
+				color = "#e535ab",
+				cterm_color = "199",
+				name = "GraphQL",
+			},
+		})
+	end,
+}
