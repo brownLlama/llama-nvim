9abf00648a107fe8b51b3c6d800b986cb7e415bf
brownLlama <inbox.sanu@gmail.com>
2024-07-01 16:50:50 +0200

    neo-tree created

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+			source_selector = {
+				winbar = false,
+				statusline = false,
+			},
+		})
+	end,
+}
