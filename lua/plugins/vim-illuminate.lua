cc10b567b43b111ae18bd20d6d8664c6d4999c29
brownLlama <inbox.sanu@gmail.com>
2024-07-01 18:10:56 +0200

    vim-illuminate added

---
+++ lua/plugins/vim-illuminate.lua
@@ -0,0 +1,5 @@
+-- Keymaps
+-- <a-n> and <a-p> as keymaps to move between references and <a-i> as a textobject for the reference illuminated under the cursor.
+return {
+  "RRethy/vim-illuminate",
+}
