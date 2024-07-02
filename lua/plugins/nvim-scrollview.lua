7f66f6079bc4d08abe2f03b60d60a6eff9eeecee
brownLlama <inbox.sanu@gmail.com>
2024-07-02 07:44:00 +0200

    lsp and mason added

--- lua/plugins/nvim-scrollview.lua
+++ lua/plugins/nvim-scrollview.lua
@@ -1,3 +1,9 @@
+-- Horizontal scroll bar on the left.
 return {
-  "dstein64/nvim-scrollview" 
+  "dstein64/nvim-scrollview",
+  config = function()
+    require("scrollview").setup({
+      signs_on_startup = {},
+    })
+  end
 }
