4132de165d3e2049d2ee52a3e6b538183e9e3a51
brownLlama <inbox.sanu@gmail.com>
2024-07-02 08:38:35 +0200

    indentation fixed

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,13 +4,13 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
-    "hrsh7th/cmp-nvim-lsp", -- source for LSP completions
-     -- snippet engine
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregex",
-    },
+		"hrsh7th/cmp-nvim-lsp", -- source for LSP completions
+		-- snippet engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregex",
+		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
